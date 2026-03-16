0c06021d4ea570feec4203f9e779490cc6d2b359
CPUPetet <c.nwodo@abujaprep.com>
2026-03-16 17:40:19 +0100

    greaterorsmaller

---
+++ greaterorsmaller.py
@@ -0,0 +1,8 @@
+number = (float(input(" Please enter a number: ")))
+
+if number < 15:
+    print("This number is less than 15.")
+elif number > 15:
+    print("This number is greater than 15.")
+else:
+    print("This number is 15.")
